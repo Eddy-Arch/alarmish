37309ed5c0b2c2dd13af8f83246896dba6a76b5d
Eddy <eddy@eddster.xyz>
2021-06-12 18:19:34 +0300

    got the basics down

--- alarmish.py
+++ alarmish.py
@@ -1,8 +1,23 @@
-#!/bin/env python3
-import sys
+#!/usr/bin/python3
+import sys, time, datetime
 from config import *
-
 c = input()
 
 if c == addalarm:
-	print("now this is epic")
+    print("what is the alarm name?\nname: ", end="")
+    alarmname = input()
+    print("alarm name will be: " + alarmname)
+    print("how many hours until the alarm goes off?\ntime: ", end="")
+    alarmduration_hours = input()
+    alarmduration_hours = int(alarmduration_hours) * 3600
+    print("how many minutes until the alarm goes off?\ntime: ", end="")
+    alarmduration_mins = input()
+    alarmduration_mins = int(alarmduration_mins) * 60
+    print("how many seconds until the alarm goes off?\ntime: ", end="")
+    alarmduration_secs = input()
+    alarmduration_secs = int(alarmduration_secs)
+    totalwaittime = alarmduration_secs + alarmduration_mins + alarmduration_hours
+    print(f'alarm will sound in {totalwaittime} seconds') 
+    time.sleep(totalwaittime)
+    print("times up!")
+
