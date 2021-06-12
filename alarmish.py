178cd73df529b7c2ba2c2aa264485613d365753d
Eddy <eddy@eddster.xyz>
2021-06-12 17:08:32 +0300

    now has a config

---
+++ alarmish.py
@@ -0,0 +1,8 @@
+#!/bin/env python3
+import sys
+from config import *
+
+c = input()
+
+if c == addalarm:
+	print("now this is epic")
---
+++ config.py
@@ -0,0 +1,4 @@
+# the following are keybinds, where the variable name describes the keybind
+# action.
+
+addalarm = 'a'
